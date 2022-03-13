aaf1361f673572ba862dcf500b32fe87df9e20f1
Eduardo Perez Pinzon <eduardoperezpinzon223@gmail.com>
2022-03-12 18:01:23 -0600

    agregar descripcion cada planeta

--- app/src/main/java/com/example/ct_listas_personalizadas/Planet.java
+++ app/src/main/java/com/example/ct_listas_personalizadas/Planet.java
@@ -3,16 +3,26 @@ package com.example.ct_listas_personalizadas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Planet extends AppCompatActivity {
-    private TextView planet;
+    private TextView planetName;
+    private TextView planetDesc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planet);
-        planet = (TextView) findViewById(R.id.planet);
-        String id = getIntent().getStringExtra("id");
-        planet.setText(id);
+
+        planetName = (TextView) findViewById(R.id.planetName);
+        planetDesc = (TextView) findViewById(R.id.planetDesc);
+
+        String description = getIntent().getStringExtra("DESC");
+        String name = getIntent().getStringExtra("NAME");
+
+        planetName.setText(name);
+        planetDesc.setText(description);
     }
 }
